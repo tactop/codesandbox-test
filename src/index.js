a5714b75b8e6333ed5eeaf79064bf0d5ef891240
tactop <34121251+tactop@users.noreply.github.com>
2021-05-19 15:51:38 +0900

    sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+// var v1 = "val1";
+// console.log(v1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let v2 = "let1";
+// console.log(v2);
+
+// v2 = "let2";
+// console.log(v2);
+
+// const v3 = "const1";
+
+// console.log(v3);
+
+const v4 = {
+  name: "ta1",
+  age: 28
+};
+console.log(v4);
+
+v4.name = "ta2";
+console.log(v4);
